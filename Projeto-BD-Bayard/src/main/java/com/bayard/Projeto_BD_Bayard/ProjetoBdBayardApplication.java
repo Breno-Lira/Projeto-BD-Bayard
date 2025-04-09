90e161a50ba6c5729dcc09f8598ced54a1d8b103
Breno-Lira <bmrl@cesar.school>
2025-04-09 14:46:42 -0300

    Adicionando umas dependencias novas =)

--- Projeto-BD-Bayard/src/main/java/com/bayard/Projeto_BD_Bayard/ProjetoBdBayardApplication.java
+++ Projeto-BD-Bayard/src/main/java/com/bayard/Projeto_BD_Bayard/ProjetoBdBayardApplication.java
@@ -1,13 +1,13 @@
-package com.BayardDB.Bayard;
+package com.bayard.Projeto_BD_Bayard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BayardApplication {
+public class ProjetoBdBayardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BayardApplication.class, args);
+		SpringApplication.run(ProjetoBdBayardApplication.class, args);
 	}
 
 }
--- Projeto-BD-Bayard/src/test/java/com/bayard/Projeto_BD_Bayard/ProjetoBdBayardApplicationTests.java
+++ Projeto-BD-Bayard/src/test/java/com/bayard/Projeto_BD_Bayard/ProjetoBdBayardApplicationTests.java
@@ -1,10 +1,10 @@
-package com.BayardDB.Bayard;
+package com.bayard.Projeto_BD_Bayard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BayardApplicationTests {
+class ProjetoBdBayardApplicationTests {
 
 	@Test
 	void contextLoads() {
